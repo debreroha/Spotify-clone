4a682841413ef68123827ead7b381d28eb286ba4
Getnet Alemye <124487134+debreroha@users.noreply.github.com>
2024-06-15 22:07:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
         token: _token,
       });
 
+      // get playlists
       s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
         dispatch({
           type: "SET_DISCOVER_WEEKLY",
